5f260905b3807b233273a14dd21d49ff23c41d89
fiirefly24 <muhammadalifikri2412@gmail.com>
2024-05-15 13:57:38 +0000

    zeus: Ship with MIUI Camera
    *Device      : https://github.com/cupid-development/android_device_xiaomi_miuicamera-cupid
    *Vendor      : https://git.mainlining.org/cupid-development/proprietary_vendor_xiaomi_miuicamera-cupid
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,6 +10,9 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/zeus/BoardConfigVendor.mk
 
+# Miui Camera for cupid
+include device/xiaomi/miuicamera-cupid/BoardConfig.mk
+
 DEVICE_PATH := device/xiaomi/zeus
 
 # Audio

76eecfdac92354b8b90fe43800034b229ace08f1
dopaemon <polarisdp@gmail.com>
2025-10-02 15:54:38 +0000

    zeus: BringUp DerpFest
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_zeus.mk
+++ lineage_zeus.mk
@@ -10,6 +10,12 @@ $(call inherit-product, device/xiaomi/zeus/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# DerpFest
+DERPFEST_BUILD_TYPE := UNOFFICIAL
+DERPFEST_BUILD_VARIANT := Stable
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_USES_PICO_GAPPS := true
+
 PRODUCT_NAME := lineage_zeus
 PRODUCT_DEVICE := zeus
 PRODUCT_MANUFACTURER := Xiaomi

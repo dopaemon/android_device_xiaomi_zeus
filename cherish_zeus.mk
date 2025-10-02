cebae005ce3491148b1319e707a2986e2fbef8d8
dopaemon <polarisdp@gmail.com>
2025-10-02 12:03:13 +0000

    zeus: BringUp CherishOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cherish_zeus.mk
+++ cherish_zeus.mk
@@ -7,10 +7,17 @@
 # Inherit from zeus device
 $(call inherit-product, device/xiaomi/zeus/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common cherish configuration
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_zeus
+# CherishOS
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_HAS_UDFPS := true
+CHERISH_BUILD_TYPE := UNOFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.cherish.maintainer=KernelPanix
+
+PRODUCT_NAME := cherish_zeus
 PRODUCT_DEVICE := zeus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
